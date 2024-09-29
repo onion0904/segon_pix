1b42d977033b5b31a473c849c599c5c51406fc1e
onion0904 <takerudaze0904@gmain.com>
2024-09-29 17:35:46 +0900

    update data struct

--- models/data_models.go
+++ models/data_models.go
@@ -1,30 +1,43 @@
 package models
 
-import(
-	"gorm.io/gorm"
+import (
+    "gorm.io/gorm"
 )
 
 type User struct {
     gorm.Model
-    Name         string         // ユーザー名
-    Profile      string         // プロフィールメッセージ
-    Icon         string         // プロフィール画像のURL
-    HeaderImage  string         // ヘッダー画像のURL
-    Email        *string        // メールアドレス（任意）
-    Age          *uint8         // 年齢（任意）
-    PostedImages []PostedImage  // ユーザーが投稿した画像
-    LikedImages  []LikedImage   // ユーザーがいいねした画像
-    Follows      []*User        `gorm:"many2many:user_follows;"` // フォロー関係（多対多）
+    Name         string        `gorm:"not null;index"` // ユーザー名
+    Profile      string        // プロフィールメッセージ
+    Icon         string        // プロフィール画像のURL
+    HeaderImage  string        // ヘッダー画像のURL
+    Email        *string       `gorm:"type:varchar(100);unique"` // メールアドレス（任意、一意のメール形式）
+    Age          *uint8        // 年齢（任意）
+    PostedImages []PostedImage // ユーザーが投稿した画像
+    LikedImages  []PostedImage // ユーザーがいいねした画像
+    Follows      []*User       `gorm:"many2many:user_follows;joinForeignKey:FollowerID;JoinReferences:FollowingID"` // フォローしているユーザー
+    Followers    []*User       `gorm:"many2many:user_follows;joinForeignKey:FollowingID;JoinReferences:FollowerID"` // フォローされているユーザー
 }
 
 type PostedImage struct {
     gorm.Model
-    URL    string `gorm:"not null"` // 画像のURL
-    UserID uint   `gorm:"not null"` // 投稿者のユーザーID
+    URL         string    `gorm:"not null"`                    // 画像のURL
+    UserID      uint      `gorm:"not null"`                    // 投稿者のユーザーID
+    PostUser    User      `gorm:"not null"`                    //投稿したユーザーの情報
+    Likes       []User    // いいねしたユーザー
+    Comments    []Comment // コメント
+    Hashtags    []Hashtag `gorm:"many2many:posted_image_hashtags;"` // ハッシュタグ
 }
 
-type LikedImage struct {
+type Comment struct {
     gorm.Model
-    URL    string `gorm:"not null"` // 画像のURL
-    UserID uint   `gorm:"not null"` // いいねしたユーザーのID
+    PostedImageID uint   `gorm:"not null"` // コメント元のPostedImageのID
+    UserID        uint   `gorm:"not null"` // コメントしたユーザーのID
+    PostUser      User   `gorm:"not null"` // コメントしたユーザーの情報
+    Message       string // コメント内容
+}
+
+type Hashtag struct {
+    gorm.Model
+    Name         string        `gorm:"unique;not null"` // ハッシュタグ名
+    PostedImages []PostedImage `gorm:"many2many:posted_image_hashtags;"` // 同じハッシュタグを持つ画像
 }
