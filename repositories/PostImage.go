08679f81e6d094808216dc6cef10474d92f14ce0
onion0904 <takerudaze0904@gmain.com>
2024-09-30 15:32:07 +0900

    upgrade repositories

--- repositories/PostImage.go
+++ repositories/PostImage.go
@@ -6,34 +6,80 @@ import (
     "io"
 	"context"
     "time"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+	"strings"
+	"net/url"
 )
 
 
 // UploadImageToGCS は画像を Google Cloud Storage にアップロードし、その URL を返します。
 func (repo *Repository) UploadImageToGCS(ctx context.Context, file io.Reader, filename string) (string, error) {
-    bucketName := "GCS_BUCKET_NAME"
+    // .envファイルの読み込み
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	bucketName := os.Getenv("GCS_BUCKET_NAME")
 	if bucketName == "" {
         return "", fmt.Errorf("GCS bucket name is not set")
     }
-    objectName := fmt.Sprintf("%v-%v", time.Now().Unix(), filename) // Unique object name
-    wc := repo.gcsClient.Bucket(bucketName).Object(objectName).NewWriter(ctx)
+    objectName := fmt.Sprintf("%v-%v", time.Now().Unix(), filename)
+    object := repo.gcsClient.Bucket(bucketName).Object(objectName)
+    wc := object.NewWriter(ctx)
+    // ファイルの書き込み
     if _, err := io.Copy(wc, file); err != nil {
+        wc.Close()
         return "", err
     }
     if err := wc.Close(); err != nil {
         return "", err
     }
-
-    return fmt.Sprintf("https://storage.googleapis.com/%s/%s", bucketName, objectName), nil
+    // オブジェクトの属性を取得してメディアリンクを取得(GCS のオブジェクトから直接 URL を取得します。)
+    attrs, err := object.Attrs(ctx)
+    if err != nil {
+        return "", err
+    }
+    return attrs.MediaLink, nil
 }
 
 // DeleteImageFromGCS は、Google Cloud Storage から画像を削除します。
-func (repo *Repository) DeleteImageFromGCS(ctx context.Context, objectName string) error {
-    bucketName := "GCS_BUCKET_NAME"
-	if bucketName == "" {
+func (repo *Repository) DeleteImageFromGCS(ctx context.Context, imageURL string) error {
+    // .envファイルの読み込み
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	bucketName := os.Getenv("GCS_BUCKET_NAME")
+    if bucketName == "" {
         return fmt.Errorf("GCS bucket name is not set")
     }
+
+    objectName, err := extractObjectNameFromURL(imageURL, bucketName)
+    if err != nil {
+        return err
+    }
+
     return repo.gcsClient.Bucket(bucketName).Object(objectName).Delete(ctx)
+}
+
+// URLからオブジェクト名を抽出する関数
+func extractObjectNameFromURL(imageURL, bucketName string) (string, error) {
+    parsedURL, err := url.Parse(imageURL)
+    if err != nil {
+        return "", fmt.Errorf("invalid image URL: %v", err)
+    }
+
+    // パスからオブジェクト名を抽出
+    parts := strings.SplitN(parsedURL.Path, "/", 3)
+    if len(parts) < 3 || parts[1] != bucketName {
+        return "", fmt.Errorf("invalid image URL path")
+    }
+
+    objectName := parts[2]
+    return objectName, nil
 }
 
 
@@ -41,77 +87,64 @@ func (repo *Repository) DeleteImageFromGCS(ctx context.Context, objectName strin
 //以下二つの関数を使う
 
 // AddPostedImageは、GCSへのアップロードを伴う投稿画像の追加を処理します。
-func (repo *Repository) AddPostedImage(ctx context.Context, file io.Reader, filename string, userID uint,hashtags []models.Hashtag) error {
+func (repo *Repository) AddPostedImage(ctx context.Context, file io.Reader, filename string, userID uint, hashtags []models.Hashtag) (err error) {
     url, err := repo.UploadImageToGCS(ctx, file, filename)
     if err != nil {
         return err
     }
 
-	tx := repo.db.Begin()
+    tx := repo.db.Begin()
     defer func() {
         if r := recover(); r != nil {
             tx.Rollback()
+            panic(r)
+        } else if err != nil {
+            tx.Rollback()
+        } else {
+            err = tx.Commit().Error
         }
     }()
 
-	var user models.User
-    if err := tx.First(&user, userID).Error; err != nil {
-        tx.Rollback()
+    var user models.User
+    if err = tx.First(&user, userID).Error; err != nil {
         return err
     }
 
     image := &models.PostedImage{
-        URL:    url,
-        UserID: userID,
-		PostUser: user,
-		Hashtags: hashtags,
+        URL:      url,
+        UserID:   userID,
+        PostUser: user,
+        Hashtags: hashtags,
     }
-    if err := repo.db.Create(image).Error; err != nil {
-		tx.Rollback()
-		return err
-	}
-
-	if err := tx.Model(&user).Association("PostedImages").Append(&image); err != nil {
-        tx.Rollback()
+    if err = tx.Create(image).Error; err != nil {
         return err
     }
 
-	return tx.Commit().Error
+    if err = tx.Model(&user).Association("PostedImages").Append(image); err != nil {
+        return err
+    }
+
+    return err
 }
 
 // DeletePostedImageは、GCSの投稿画像と対応するファイルの削除を処理します。
 func (repo *Repository) DeletePostedImage(ctx context.Context, imageID uint) error {
-    var image models.PostedImage
-    if err := repo.db.First(&image, imageID).Error; err != nil {
-        return err
-    }
-
-    if err := repo.DeleteImageFromGCS(ctx, image.URL); err != nil {
-        return err
-    }
-
-	tx := repo.db.Begin()
-    defer func() {
-        if r := recover(); r != nil {
-            tx.Rollback()
+    return repo.db.Transaction(func(tx *gorm.DB) error {
+        var image models.PostedImage
+        if err := tx.First(&image, imageID).Error; err != nil {
+            return err
         }
-    }()
 
-	var user models.User
-    if err := tx.First(&user, image.UserID).Error; err != nil {
-        tx.Rollback()
-        return err
-    }
+        // GCSから画像を削除
+        if err := repo.DeleteImageFromGCS(ctx, image.URL); err != nil {
+            return err
+        }
 
-    if err := tx.Delete(&image).Error; err != nil {
-		tx.Rollback()
-		return err
-	}
+        // 画像を削除（関連付けも自動的に処理される）
+        if err := tx.Delete(&image).Error; err != nil {
+            return err
+        }
 
-	if err := tx.Model(&user).Association("PostedImages").Delete(&image); err != nil {
-        tx.Rollback()
-        return err
-    }
-
-	return tx.Commit().Error
+        return nil
+    })
 }
--- repositories/User.go
+++ repositories/User.go
@@ -2,28 +2,70 @@ package repositories
 
 import (
 	"PixApp/models"
+	"errors"
+	"gorm.io/gorm"
+	"context"
 )
 
 // 与えられたidのユーザー情報を返す
-func (repo Repository) UserInfo (id uint) (models.User,error) {
-	user := models.User{}
+func (repo *Repository) UserInfo(id uint) (*models.User, error) {
+    var user models.User
     if err := repo.db.First(&user, id).Error; err != nil {
-        return user, err
+        if errors.Is(err, gorm.ErrRecordNotFound) {
+            return nil, nil // またはカスタムエラーを返す
+        }
+        return nil, err
     }
-    return user, nil
+    return &user, nil
 }
+
 
 // 与えられたハッシュタグの部分一致の画像のスライスを返す
 func (repo *Repository) ImageList(Qhashtag string) ([]models.PostedImage, error) {
-	var images []models.PostedImage
-	err := repo.db.
-		Model(&models.PostedImage{}).
-		Joins("JOIN posted_image_hashtags on posted_image_hashtags.posted_image_id = posted_images.id").
-		Joins("JOIN hashtags on posted_image_hashtags.hashtag_id = hashtags.id").
-		Where("hashtags.name LIKE ?", "%"+Qhashtag+"%").
-		Find(&images).Error
-	if err != nil {
-		return nil, err
-	}
-	return images, nil
+    var images []models.PostedImage
+    err := repo.db.
+        Preload("PostUser").
+        Preload("Likes").
+        Preload("Comments").
+        Preload("Hashtags").
+        Joins("JOIN posted_image_hashtags ON posted_image_hashtags.posted_image_id = posted_images.id").
+        Joins("JOIN hashtags ON posted_image_hashtags.hashtag_id = hashtags.id").
+        Where("hashtags.name LIKE ?", "%"+Qhashtag+"%").
+        Find(&images).Error
+    if err != nil {
+        return nil, err
+    }
+    return images, nil
 }
+
+
+func (repo *Repository) AddUser(model *models.User) error {
+    return repo.db.Create(model).Error
+}
+
+
+func (repo *Repository) DeleteUser(userID uint) error {
+    return repo.db.Transaction(func(tx *gorm.DB) error {
+        // ユーザーを取得し、投稿画像をプリロード
+        var user models.User
+        if err := tx.Preload("PostedImages").First(&user, userID).Error; err != nil {
+            return err
+        }
+
+        // ユーザーの投稿画像をGCSから削除
+        for _, image := range user.PostedImages {
+            // GCSから画像を削除
+            if err := repo.DeletePostedImage(context.Background(), image.ID); err != nil {
+                return err
+            }
+        }
+
+        // ユーザーを削除
+        if err := tx.Delete(&user).Error; err != nil {
+            return err
+        }
+
+        return nil
+    })
+}
+
