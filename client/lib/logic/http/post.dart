ec9be7cbdc19a29e81a339d3b215059fe0538fe9
seg <segnities007@gmail.com>
2024-10-18 22:00:59 +0900

    modify verify

--- client/lib/logic/http/post.dart
+++ client/lib/logic/http/post.dart
@@ -1,9 +1,8 @@
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:image_picker/image_picker.dart';
-import 'package:http_parser/http_parser.dart';
-import 'package:mime/mime.dart';
-import '../../model/user.dart';
+// import 'package:http_parser/http_parser.dart';
+// import 'package:mime/mime.dart';
 
 Future<http.Response> createUser({
   required String name,
@@ -13,19 +12,19 @@ Future<http.Response> createUser({
   required int birthday,
   required String token,
 }) async {
-  final response =
-      await http.post(Uri.parse("http://localhost:8080/segon_pix_auth/add/user"),
-          headers: {
-            "Content-Type": "application/json; charset=UTF-8",
-            "Authorization": "Bearer $token"
-          },
-          body: jsonEncode({
-            "Name": name,
-            "Description": description,
-            "Email": email,
-            "Password": password,
-            "birthday": birthday,
-          }));
+  final response = await http.post(
+      Uri.parse("http://localhost:8080/segon_pix_auth/add/user"),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+        "Authorization": "Bearer $token"
+      },
+      body: jsonEncode({
+        "Name": name,
+        "Description": description,
+        "Email": email,
+        "Password": password,
+        "birthday": birthday,
+      }));
   if (response.statusCode == 200) {
     return response;
   } else {
@@ -34,29 +33,32 @@ Future<http.Response> createUser({
 }
 
 Future<http.StreamedResponse> postImage({
-  required List<String> hashTags,
+  required String hashTags,
   required int userId,
   required XFile imageFile,
+  required String token,
 }) async {
-  var uri = Uri.http(
+  final uri = Uri.http(
     "localhost:8080",
     "/segon_pix/add/image",
     {"ID": "$userId"},
   );
-  var request = http.MultipartRequest('POST', uri);
-  var mimeTypeData =
-      lookupMimeType(imageFile.path, headerBytes: [0xFF, 0xD8])?.split('/');
-  request.files.add(
-    await http.MultipartFile.fromPath(
-      'File',
-      imageFile.path,
-      contentType: MediaType(mimeTypeData![0], mimeTypeData[1]),
-    ),
-  );
-  for (var tag in hashTags) {
-    request.fields['Hashtags'] = tag;
+
+  var request = http.MultipartRequest(
+    'POST',
+    uri,
+  )
+    ..headers["Authorization"] = "Bearer $token"
+    ..files.add(await http.MultipartFile.fromPath("File", imageFile.path))
+    ..fields["Hashtags"] = hashTags;
+
+
+    final response =  await request.send();
+    if(response.statusCode == 200){
+      return response;
+    }else {
+    throw Exception("Failed postImage lib/logic/http/post.dart");
   }
-  return await request.send();
 }
 
 Future<http.Response> addLike({
