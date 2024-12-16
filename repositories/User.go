273014789f807b35fbee5c87a34fd9605553047e
onion0904 <takerudaze0904@gmain.com>
2024-12-17 00:51:45 +0900

    move ExistUser from repo/auth to repo/User and delete repo/auth

--- repositories/User.go
+++ repositories/User.go
@@ -149,3 +149,22 @@ func (repo *Repository) UpdateUserInfo(userID uint, name ,description , email st
 
     return nil
 }
+
+func (repo *Repository) ExistUser(email string, password string) (uint,error) {
+    var user models.User
+    // Emailでユーザーを検索
+    if err := repo.db.
+        Where("email = ?", email).First(&user).Error; err != nil {
+        if errors.Is(err, gorm.ErrRecordNotFound) {
+            return 0,nil
+        }
+        return 0,err
+    }
+
+    // パスワードの照合
+    if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
+        return 0,errors.New("invalid password")
+    }
+
+    return user.ID,nil
+}
